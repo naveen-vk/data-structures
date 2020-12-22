89a44dbff1cb1eee6512ec6934aa03f9c9339a65
Naveen Vijayakumar <naveen.vijayakumar@examly.in>
2020-12-22 20:31:48 +0530

    Question 1

--- Arrays/Question1.java
+++ Arrays/Question1.java
@@ -1,7 +1,38 @@
 import java.util.*;
 
 public class Question1{
+
+    static void isUnique(String str)
+    {
+        int len=str.length();
+        int count=0;
+        for(int i=0;i<len;i++)
+        {
+            for(int j=i+1;j<len;j++)
+            {
+                if(str.charAt(i)==str.charAt(j))
+                {
+                    count++;
+                }
+            }
+        }
+        if(count>0)
+        {
+            System.out.println("Not Unique");
+        }
+        else
+        {
+            System.out.println("Unique");
+        }
+    }
     public static void main(String[] args){
-        System.out.println("Hello");
+        isUnique("Hello World"); 
+        isUnique("mobile");
+        isUnique("watch");
+        isUnique("hello");
+        isUnique("123456");
+        isUnique("@!1234");
+        isUnique("!@#$%^&*()_+");
+        isUnique("Naveen");
     }
 }
