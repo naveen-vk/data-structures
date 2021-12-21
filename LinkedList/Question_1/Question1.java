24562156359a523d64e09a9ef2f454addff1e3cc
Naveen17euit093 <17euit093@skcet.ac.in>
2021-12-20 20:48:50 -0600

    20/12/2021

--- LinkedList/Question_1/Question1.java
+++ LinkedList/Question_1/Question1.java
@@ -1,7 +1,6 @@
 import java.util.HashSet;
 import java.util.Set;
- 
-// A Linked List Node
+
 class Node
 {
     int data;
@@ -16,7 +15,7 @@ class Node
  
 class Question1
 {
-    // Helper function to print a given linked list
+
     public static void printList(Node head)
     {
         Node ptr = head;
@@ -28,24 +27,19 @@ class Question1
         System.out.println("null");
     }
  
-    // Function to remove duplicates from a sorted list
     public static Node removeDuplicates(Node head)
     {
         Node previous = null;
         Node current = head;
- 
-        // take an empty set to store linked list nodes for future reference
-       
- 
-        // do till the linked list is empty
+
         while (current != null)
         {
-            // if the current node is seen before, ignore it
+
             if (set.contains(current.data)) {
                 previous.next = current.next;
             }
             else {
-                // insert the current node into the set and proceed to the next node
+ 
                 set.add(current.data);
                 previous = current;
             }
@@ -57,20 +51,19 @@ class Question1
  
     public static void main(String[] args)
     {
-        // input keys
+
         int[] keys = {5, 3, 4, 2, 5, 4, 1, 3};
  
-        // points to the head node of the linked list
+
         Node head = null;
  
-        // construct a linked list
         for (int i = keys.length - 1; i >= 0; i--) {
             head = new Node(keys[i], head);
         }
  
         removeDuplicates(head);
  
-        // print linked list
+
         printList(head);
     }
 }
--- LinkedList/Question_4/Question4.java
+++ LinkedList/Question_4/Question4.java
@@ -1,3 +1,21 @@
-public class Question4 {
-    
-}
+Node partition(Node head,int k)
+    {
+        Node firstHalf = null;
+        Node secondHalf = null;
+        Node ptr1 = firstHalf;
+        Node ptr2 = secondHalf;
+        Node ptr = head;
+        while(ptr != null){
+            if(ptr.data < k){
+                ptr1.data = ptr.data;
+                ptr1 = ptr1.next;
+            }
+            else {
+                ptr2.data = ptr.data;
+                ptr2 = ptr2.next;
+            }
+            ptr = ptr.next;
+        }
+        ptr1 = secondHalf;
+        return firstHalf;
+    }
